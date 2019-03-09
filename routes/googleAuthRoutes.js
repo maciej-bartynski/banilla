6d4e2368dafa3faf3d00477c4023da8f7882c2f1
Maciej Bartyński <maciek.bartynski@gmail.com>
2019-03-10 00:37:27 +0100

    respondeDaat

--- routes/googleAuthRoutes.js
+++ routes/googleAuthRoutes.js
@@ -4,6 +4,7 @@ module.exports = (app) => {
     app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
     app.get('/auth/google/callback', passport.authenticate('google'));
     app.get('/current-user', (req, res)=>{
-        res.send(req.user)
+        let responseData = req.user ? req.user : {"response":"no user"}
+        res.send(responseData)
     })
 }
