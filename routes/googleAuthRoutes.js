bd74d0918a580b0ed41cc5c542844083c155eea3
Maciej Bartyński <maciek.bartynski@gmail.com>
2019-03-10 17:55:29 +0100

    change user done

--- routes/googleAuthRoutes.js
+++ routes/googleAuthRoutes.js
@@ -1,8 +1,22 @@
 const passport = require('passport');
 
+const googleAuthOptions = {
+    scope: ['profile', 'email'],
+    prompt: ['select_account']
+}
+
 module.exports = (app) => {
 
-    app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+    app.get('/auth/google', passport.authenticate('google',  googleAuthOptions));
+
+    app.get('/change-user', (req, res)=>{
+        if (req.user) {
+            req.logout();
+            res.redirect('/auth/google');
+            return;
+        }
+        res.redirect('/auth/google');
+    })
 
     app.get('/auth/google/callback', passport.authenticate('google'), (req, res) => {
         res.redirect('/current-user');
