06fa238b39253e9344a44f5a92a63b126abaf358
Maciej Bartyński <maciek.bartynski@gmail.com>
2019-03-10 11:07:30 +0100

    login/logout/revisit flow

--- routes/googleAuthRoutes.js
+++ routes/googleAuthRoutes.js
@@ -2,11 +2,12 @@ const passport = require('passport');
 
 module.exports = (app) => {
     app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
     app.get('/auth/google/callback', passport.authenticate('google'), (req, res) => {
         res.redirect('/current-user');
     });
 
-    app.get('/logout', (req, res)=>{
+    app.get('/logout', (req, res) => {
         req.logout();
         let responseData = req.user ? req.user : "U R logged out"
         res.send(responseData)
@@ -14,6 +15,7 @@ module.exports = (app) => {
 
     app.get('/current-user', (req, res) => {
         let responseData = req.user ? req.user : "no current user"
-        res.send(responseData)
+        res.send({ 'You are logged as': responseData })
     })
+
 }
--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -1,5 +1,20 @@
 module.exports = (app) => {
     app.get('/', (req, res) => {
+        if (req.user) {
+            res.redirect('/most-recent-user');
+            return;
+        }
         res.send('Go to "/auth/google" to login. There will be redirect button soon.');
     });
+
+    app.get('/most-recent-user', (req, res) => {
+        if (!req.user) {
+            res.redirect('/');
+            return;
+        }
+        res.send({
+            'You are still logged as ': req.user,
+            'You can logout at: ': '"/logout"'
+        })
+    })
 }
